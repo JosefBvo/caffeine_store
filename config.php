2fbeb9f9ce4b1aa96c310c3bbbd8350f15efe36e
Josef Bayer <josefhonda03@yahoo.com>
2025-10-22 17:11:40 -0500

    Front end test page fucntionality

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // config.php - will need updated with DB credentials
 $DB_HOST = "localhost";
 $DB_USER = "root";
-$DB_PASS = ""; 
+$DB_PASS = "root"; 
 $DB_NAME = "caffeine_store"; // change to whatever the DB is named
 
 ?>
